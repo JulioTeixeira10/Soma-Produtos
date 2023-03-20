6e49053e4f401a41e74842a1d0b95fc96d577cf8
Julio Teixeira <julioteixeira700@gmail.com>
2023-03-20 10:33:59 -0300

    .gitignore modificado, comentários adicionados e dicionário criado com produtos e quantidades do teceiro arquivo

--- somap2.py
+++ somap2.py
@@ -1,3 +1,4 @@
+#Cria um dicionário com os produtos e suas devidas quantidades da primeira lista
 with open("C:\\Users\\Usefr\\Desktop\\Soma-Produtos\\t1.txt") as t1:
     produtos1 = {}
     n = 0
@@ -11,6 +12,7 @@ with open("C:\\Users\\Usefr\\Desktop\\Soma-Produtos\\t1.txt") as t1:
             produtos1[nomeproduto] = quantidade
             n += 1
 
+#Cria um dicionário com os produtos e suas devidas quantidades da segunda lista
 with open("C:\\Users\\Usefr\\Desktop\\Soma-Produtos\\t2.txt") as t2:
     produtos2 = {}
     n2 = 0
@@ -27,6 +29,24 @@ with open("C:\\Users\\Usefr\\Desktop\\Soma-Produtos\\t2.txt") as t2:
                 produtos2[nomeproduto] = quantidade
             n2 += 1
 
+#Cria um dicionário com os produtos e suas devidas quantidades da teceira lista
+with open("C:\\Users\\Usefr\\Desktop\\Soma-Produtos\\t3.txt") as t3:
+    produtos3 = {}
+    n = 0
+    for row in t3:
+        if (n % 2) == 0:
+            nomeproduto = row.strip()
+            n += 1
+        else:
+            stripquantidade = row[0:2].strip()
+            quantidade = int(stripquantidade)
+            if nomeproduto in produtos3:
+                produtos3[nomeproduto] += quantidade
+            else:
+                produtos3[nomeproduto] = quantidade
+            n += 1
+
+#Cria um arquivo que soma os produtos em comúm entre o arquivo t1 e t2 e outro com os produtos únicos do arquivo t1
 for produto in produtos1:
     if produto in produtos2:
         resquantidade = produtos1[produto] + produtos2[produto]
@@ -43,7 +63,7 @@ for produto in produtos1:
         file2.write(str(produtos1[produto]))
         file2.write("\n\n")
 
-
+#Cria um arquivo com os produtos únicos do arquivo t2
 for produto in produtos2:
     if produto in produtos1:
         pass
